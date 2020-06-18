769a96a04fcf8218b4a515b299aabbfe69260992
CaptainCharlieGreen <cgreen1888@gmail.com>
2020-06-17 19:19:33 -0700

    adding summon experience

--- index.js
+++ index.js
@@ -1,15 +1,14 @@
-const { Client, MessageEmbed } = require('discord.js');
-const moment = require('moment');
+const { Client } = require('discord.js');
 const client = new Client();
 const tokens = require('./tokens.json');
 const { locations } = require('./config');
-const messageCollector = require('./messageCollector');
-const { setupRoles, getUsersForLocation } = require('./userRoles');
+const { setupRoles } = require('./userRoles');
+const { summonRequest } = require('./summonRequest');
 const log = msg => console.log(msg)
 const locationNames = new Set(locations.map(loc => loc.name).map(s => s.toLowerCase()))
 
 client.on('ready', () => {
-	// console.log(`Logged in as ${client.user.tag}!`);
+	console.log(`Logged in as ${client.user.tag}!`);
 });
 
 client.on('message', async message => {
@@ -22,53 +21,11 @@ client.on('message', async message => {
 			setupRoles(message, false);
 		} else if (!locationNames.has(locationOrSetup)) {
 			message.reply(`Incorrect syntax. Type "!scb location" where location is one of:
-    ${Array.from(locations).join(', ')}`)
-			return;
+    ${Array.from(locations).map(l => l.name).join(', ')}`)
 		} else {
-			const embed = new MessageEmbed()
-			.setTitle(`Summoning for ${locationOrSetup.toUpperCase()} @ ${moment().format('hh:mm')}`)
-			// .setDescription()
-
-			const users = await getUsersForLocation(message, locations
-				.filter(loc => loc.name.toLowerCase() === locationOrSetup.toLowerCase())[0].emoji);
-
-			log(users)
-			message.channel.send(embed)
-				.then(message => {
-					messageCollector(message, x => true)
-					// const collector = message.createReactionCollector(filter, { time: 15000 });
-
-					// collector.on('collect', (reaction, user) => {
-					// 	console.log(`Collected ${reaction.emoji.name} from ${user.tag}`);
-					// });
-
-					// collector.on('end', collected => {
-					// 	console.log(`Collected ${collected.size} items`);
-					// });
-				})
-			}
-		// if (user) {
-		// 	console.log(user)
-		// }
-	}
-})
-
-client.on('messageReactionAdd', async (reaction, user) => {
-	// When we receive a reaction we check if the reaction is partial or not
-	if (reaction.partial) {
-		// If the message this reaction belongs to was removed the fetching might result in an API error, which we need to handle
-		try {
-			await reaction.fetch();
-		} catch (error) {
-			console.log('Something went wrong when fetching the message: ', error);
-			// Return as `reaction.message.author` may be undefined/null
-			return;
+			summonRequest(message, locationOrSetup)
 		}
 	}
-	// // Now the message has been cached and is fully available
-	// console.log(`${reaction.message.author}'s message "${reaction.message.content}" gained a reaction!`);
-	// // The reaction is now also fully available and the properties will be reflected accurately:
-	// console.log(`${reaction.count} user(s) have given the same reaction to this message!`);
 });
 
 client.login(tokens.botToken);
---
+++ summonRequest.js
@@ -0,0 +1,65 @@
+const { MessageEmbed } = require('discord.js');
+const moment = require('moment');
+const { locations } = require('./config');
+const { getUsersForLocation } = require('./userRoles');
+
+
+const emojis = {
+	"Summoners": '🧙‍♀️',
+	"Clickers": '⚙',
+	pendingSummon: '🌌',
+	alreadySummoned: '✅'
+}
+
+function getNamesFor (reactions, emoji) {
+	const users = Array.from(reactions.cache.get(emoji).users.cache.values())
+	return users.filter(u => !u.bot)
+		.map(u => u.username);
+}
+
+async function populatePersonel (message, serviceUsers, location, origTime) {
+	await message.reactions.resolve();
+	const reactions = message.reactions;
+	const toSummon = getNamesFor(reactions, emojis.pendingSummon);
+	const alreadySummoned = new Set(getNamesFor(reactions, emojis.alreadySummoned))
+	const summonText = toSummon
+		.map(s => `${s} ${alreadySummoned.has(s) ? emojis.alreadySummoned: ""}`)
+		.join('\n');
+	const fields = [
+		{ name: "Summoners", value: getNamesFor(reactions, emojis.Summoners).join('\n') || "-", inline: true},
+		{ name: "Clickers", value: getNamesFor(reactions, emojis.Clickers).join('\n') || "-", inline: true},
+		{ name: "Passengers", value:  summonText || "-", inline: true},
+	]
+	const embed = new MessageEmbed()
+		.setTitle(`Summoning for ${location.toUpperCase()} @ ${origTime}`)
+		.addFields(fields)
+		.setFooter('---------------------------------------------------------------------------------');
+	message.edit(`${location} summon request ${serviceUsers.map(u => `<@${u.id}>`).join(' ')}`, embed);
+}
+
+async function summonRequest(message, location) {
+	const time = moment().format('hh:mm')
+	const embed = new MessageEmbed()
+		.setTitle(`Summoning for ${location.toUpperCase()} @ ${time}`)
+
+	const users = await getUsersForLocation(message, locations
+		.filter(loc => loc.name.toLowerCase() === location.toLowerCase())[0].emoji);
+	const allUsers = users.summoners.concat(users.clickers);
+	
+	const msg = await message.channel.send(embed)
+	for(key in emojis) {
+		await msg.react(emojis[key]);
+	}
+	function redrive () {
+		populatePersonel(msg, allUsers, location, time)
+	}
+	const collector = msg.createReactionCollector(x=>true, {time: (1000 * 60 * 60), remove: true});
+	
+	collector.on('collect', redrive);
+
+	redrive();
+}
+
+module.exports = {
+	summonRequest: summonRequest
+};
